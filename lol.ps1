441d12b055a96a34b9ca35dddf608f9fa8917386
Lukarusja <lucar6170@gmail.com>
2022-01-26 22:59:40 +0000

    what am i doing2

--- lol.ps1
+++ lol.ps1
@@ -1,5 +1,7 @@
 cd C:\Users\wladadmin
 
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+
 #Download AzCopy
 Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile AzCopy.zip -UseBasicParsing
  
