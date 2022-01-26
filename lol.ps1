8a2ec9e8f149db6c7df6a4c5a84494edfeb83d3a
Lukarusja <lucar6170@gmail.com>
2022-01-26 18:27:07 +0000

    initial commit

---
+++ lol.ps1
@@ -0,0 +1,22 @@
+cd C:\Users\wladadmin
+
+#Download AzCopy
+Invoke-WebRequest -Uri "https://aka.ms/downloadazcopy-v10-windows" -OutFile AzCopy.zip -UseBasicParsing
+ 
+#Curl.exe option (Windows 10 Spring 2018 Update (or later))
+curl.exe -L -o AzCopy.zip https://aka.ms/downloadazcopy-v10-windows
+ 
+#Expand Archive
+Expand-Archive ./AzCopy.zip ./AzCopy -Force
+ 
+#Move AzCopy to the destination you want to store it
+Get-ChildItem ./AzCopy/*/azcopy.exe | Move-Item -Destination "C:\Users\wladadmin\AzCopy\AzCopy.exe"
+ 
+#Add your AzCopy path to the Windows environment PATH (C:\Users\wladadmin\AzCopy in this example), e.g., using PowerShell:
+$userenv = [System.Environment]::GetEnvironmentVariable("Path", "User")
+[System.Environment]::SetEnvironmentVariable("PATH", $userenv + ";C:\Users\wladadmin\AzCopy", "User")
+
+Start-Sleep -s 3
+
+azcopy login --identity
+azcopy copy "https://wladkeysstorage.blob.core.windows.net/keys/*" "C:\Users\wladadmin" --recursive=true
